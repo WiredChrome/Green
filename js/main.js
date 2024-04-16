be202f96dbed7e8ff9269c696299a1d672e1b136
Rohit Sarkar <137258920+WiredChrome@users.noreply.github.com>
2024-04-16 09:48:18 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ const logo1 = document.getElementById('logo1');
 const logo2 = document.getElementById('logo2');
 
 window.addEventListener('scroll', function() {
-  if (window.scrollY > window.innerHeight * 0.1) { // 10% of viewport height
-    logo1.classList.add('logo-fade-out'); // Apply the fade-out class
-    logo2.style.opacity = '1'; // Make logo2 fully visible
+  if (window.scrollY > window.innerHeight * 0.1) {
+    logo1.classList.add('logo-fade-out');
+    logo2.style.opacity = '1';
   } else {
     logo1.classList.remove('logo-fade-out');
-    logo2.style.opacity = '0'; // Hide logo2 again
+    logo2.style.opacity = '0';
   }
 });
